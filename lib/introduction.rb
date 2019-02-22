40284fc89ff692feb40efd0dfccc05261f52ec41
Tommy Lau <tlau425@yahoo.com>
2019-02-22 01:10:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,3 @@ end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
-
-
-introduction("Josh")
-introduction_with_language()
